5e5ebb37b46883bf932cdc6ae651a5620f45d213
colemonnahan <monnahc@uw.edu>
2018-07-06 17:53:20 -0400

    Agrega los archivos por el model swallows

---
+++ run_swallows.R
@@ -0,0 +1,20 @@
+## Este codigo correr la version original del modelo "swallows" que es un
+## CJS modelo. Examina el archivo swallows.stan para ver documentacion del
+## modelo. Vamos a usarlo como el punto de partida de construir nuestro
+## modelo.
+library(TMB)
+
+data <- readRDS('datos/swallows_datos.RDS')
+inits <- list(
+  sigmayearphi=.7, sigmaphi=.5, sigmap=.9, a=rep(3.5, len=data$K-1), a1=0,
+  b0=rep(2, len=4), b1=rep(0, len=4), fameffphi_raw=rep(0, len=data$nfam),
+  fameffp_raw=rep(0, len=data$nfam), yeareffphi_raw=rep(0, len=4))
+
+## Compile, link and build model
+compile('modelos/swallows.cpp')
+dyn.load(dynlib('modelos/swallows'))
+obj <- MakeADFun(data=data, parameters=inits,
+                 random=c('fameffphi_raw', 'fameffp_raw', 'yeareffphi_raw'))
+## Adjustar con efectos aleatorios
+opt <- nlminb(obj$par, obj$fn, obj$gr)
+opt$par
