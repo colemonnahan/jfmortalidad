bbe69b905cb2085ab2e3d960f88048afbda26f01
colemonnahan <monnahc@uw.edu>
2018-07-12 13:46:45 -0400

    Parte el codigo de la simulacion y actualiza

---
+++ run_simulation.R
@@ -0,0 +1,41 @@
+library(TMB)
+compile('modelos/cjs_jf.cpp')
+dyn.load(dynlib('modelos/cjs_jf'))
+
+### Ahora simulamos datos similares que los reales y los adjustamos. Tienes
+### que correr el codigo abajo primero.
+nrep <- 200
+coverage.list <- results.list <- list()
+## no se puede usar la variable 'i' porque es usado en el archivo
+## simulator.R
+for(ii in 1:nrep){
+  print(ii)
+  set.seed(ii)
+  source("simulator.R")
+  ## plot(simdata$last, main='Simulated data')
+  ## adjusta el modelo con los datos simulados
+  obj <- MakeADFun(data=simdata, parameters=simpars, DLL='cjs_jf')
+  obj$env$beSilent()
+  opt <- nlminb(obj$par, obj$fn, obj$gr, control=list(trace=0))
+  rep <- sdreport(obj)
+  est <- rep$par.fixed
+  se <- sqrt(diag(rep$cov.fixed))
+  true <- unlist(simpars)
+  ## verifica que sirve
+  results.list[[ii]] <- data.frame(rep=ii,par=names(se), true=true, est=est, se=se,
+                    covered= (true-2*se < est & est < true+2*se))
+  coverage.list[[ii]] <- c((true-2*se < est & est < true+2*se))
+}
+
+## los parametros estimados deberian contener el valor de la simulacion
+## aproximadamente 95% de los replicacions
+apply(do.call(rbind, coverage.list), 2, mean)
+
+## lo visualizar
+library(ggplot2)
+results <- do.call(rbind, results.list)
+ggplot(data=results) +
+  geom_linerange(aes(x=rep, ymin=est-1.96*se, ymax=est+1.96*se)) +
+  geom_point(aes(x=rep, y=est))+
+  geom_hline(aes(yintercept=true), col='red')+
+  facet_wrap('par')
--- run_swallows.R
+++ run_swallows.R
@@ -6,26 +6,29 @@
 library(TMB)
 data <- readRDS('datos/swallows_datos.RDS')
 inits <- list(
-  sigmayearphi=.7, sigmaphi=.5, sigmap=.9, a=rep(3.5, len=data$K-1), a1=0,
+  sigmayearphi=.7, sigmaphi=.5, sigmap=.9, a=rep(-3.5, len=data$K-1), a1=0,
   b0=rep(2, len=4), b1=rep(0, len=4), fameffphi_raw=rep(0, len=data$nfam),
   fameffp_raw=rep(0, len=data$nfam), yeareffphi_raw=rep(0, len=4))
 ## Compile, link and build model
 compile('modelos/swallows.cpp')
 dyn.load(dynlib('modelos/swallows'))
-obj <- MakeADFun(data=data, parameters=inits,
+obj <- MakeADFun(data=data, parameters=inits, DLL='swallows',
                  random=c('fameffphi_raw', 'fameffp_raw', 'yeareffphi_raw'))
 ## Adjustar con efectos aleatorios
 opt <- nlminb(obj$par, obj$fn, obj$gr)
-
+## Use these in the simulation
+pars0 <- opt$par
+rep0 <- obj$report()
 
 ### Ahora simulamos datos similares que los reales y los adjustamos. Tienes
 ### que correr el codigo abajo primero.
 source("simulator.R")
 par(mfrow=c(1,3))
 ## chequea que son similares
+true <- unlist(simpars[1:7])
 plot(data$last, main='Real data')
 plot(last, main='Simulated data')
-plot(opt$par, as.vector(unlist(simpars[1:7])), main='Parameters',
+plot(opt$par, true, main='Parameters',
      xlab='Real', ylab='Simulated')
 abline(0,1)
 ### adjusta el modelo con los datos simulados
@@ -37,9 +40,8 @@ opt <- nlminb(obj$par, obj$fn, obj$gr)
 rep <- sdreport(obj)
 est <- rep$par.fixed
 se <- sqrt(diag(rep$cov.fixed))
-true <- unlist(simpars[1:7])
 ## verifica que sirve
-out <- data.frame(par=names(se), true=true, est=est,
+out <- data.frame(par=names(se), true=true, est=est, se=se,
                   covered= (true-2*se < est & est < true+2*se))
 out
 mean(out$covered)
