92d254ca32291c1174f948bce531a530200dc034
colemonnahan <monnahc@uw.edu>
2019-01-22 21:08:34 -0300

    Simplifica el modelo tener efectos aleatorios por periodos

--- modelos/cjs_jf.cpp
+++ modelos/cjs_jf.cpp
@@ -38,10 +38,14 @@ Type objective_function<Type>::operator() ()
   //  PARAMETER(logk);		// effecto de esfuerza de probabilidad de captura
   PARAMETER(a); 		// efecto de la selectividad
   PARAMETER(b); 		// efecto de la selectividad
-  PARAMETER_ARRAY(tau);		// efectos aleatorios por k por machos
-  PARAMETER_MATRIX(mu_tau);	// los promedios de los efectos aleatorios
+  //  PARAMETER_ARRAY(tau);		// efectos aleatorios por k por machos
+  PARAMETER_VECTOR(tau); 
+  // PARAMETER_MATRIX(mu_tau);	// los promedios de los efectos aleatorios
+  PARAMETER(mu_tau);
   PARAMETER(logsigma_tau);	// la varianza de todos los efectos aleatorios
   PARAMETER(theta); 		// la maxima probabilidad de captura
+  PARAMETER(logM_mu);
+  PARAMETER(logM_sigma);
   Type nll=0.0;			// negativa log verosimiltud
   matrix<Type> p(I,K); 		// probabilidad de las capturas
   matrix<Type> phi(I,K);	// prob. de la sobrevivencia
@@ -52,35 +56,38 @@ Type objective_function<Type>::operator() ()
   chi.setZero();
 
   int kk;
-  vector<Type> NatM;
-  Type max_prob=inv_logit(theta);
-
+  vector<Type> NatM(K);
+  vector<Type> pcap(K);
   NatM=exp(logNatM);
+  Type max_prob=inv_logit(theta);
   Type r=exp(logr);
   Type sigma_tau=exp(logsigma_tau);
   // la primero columna es por machos y la segunda por hembras
   array<Type> k(K,2,3); // periodos por generos por eventos
   k.setZero();
   for(int t=0;t<K;t++) { // periodos
-    for(int ev=0; ev<3; ev++){ // eventos
-    // non-centered efectos aleatorios
-      k(t,0,ev)=exp(tau(t,0,ev)*sigma_tau+mu_tau(0,ev)); // machos
-      k(t,1,ev)=exp(tau(t,1,ev)*sigma_tau+mu_tau(1,ev)); // hembras
-    }
+    // for(int ev=0; ev<3; ev++){ // eventos
+    // // non-centered efectos aleatorios
+    //   k(t,0,ev)=exp(tau(t,0,ev)*sigma_tau+mu_tau(0,ev)); // machos
+    //   k(t,1,ev)=exp(tau(t,1,ev)*sigma_tau+mu_tau(1,ev)); // hembras
+    // }
+    pcap(t)=inv_logit(tau(t));
   }
 
   // TMB usa indices de 0, no de 1, entoces tenemos que estar cuidadoso, y
   // uso un "-1" para ser claro que pasa.
   for(int i=0; i<I; i++){ // iterando sobre cada individuos
     // inicializacion estan vivo en periodo uno
-    phi(i,first(i)-1)=exp(-NatM(sexo(i))-counts(i,first(i)-1)*r);
+    //    phi(i,first(i)-1)=exp(-NatM(sexo(i))-counts(i,first(i)-1)*r);
+    phi(i,first(i)-1)=exp(-NatM(0)-counts(i,first(i)-1)*r);
     p(i,first(i)-1)=1;
     for(int t=first(i); t<K; t++) {
       // calcular prob. capturas como una funcion de efectos y covariables
-      p(i,t) =max_prob*(1-exp(-k(t,sexo(i),evento(i))*effort(t)))/(1+exp(-a*(lengths(i)-b)));
+      // p(i,t) =max_prob*(1-exp(-k(t,sexo(i),evento(i))*effort(t)))/(1+exp(-a*(lengths(i)-b)));
+      p(i,t)=pcap(t);
       // calcular prob. sobrevivencia como una funcion de efectos y
       // covariables
-      phi(i,t) = exp(-NatM(sexo(i))-counts(i,t-1)*r);
+      phi(i,t) = exp(-NatM(t)-counts(i,t-1)*r);
     }
     // la probabilidad de no ser visto nunca mas, usa un indice reverso
     // para calcular hacia atras usando recursion.
@@ -117,18 +124,20 @@ Type objective_function<Type>::operator() ()
 
   // // Probabilidad de los efectos aleatorios
   for(int t=0;t<K;t++) { // periodos
-    for(int ev=0; ev<3; ev++){ // eventos
-      nll-=dnorm(tau(t,0,ev), Type(0), Type(1), true);
-      nll-=dnorm(tau(t,1,ev), Type(0), Type(1), true);
-    }
+    // for(int ev=0; ev<3; ev++){ // eventos
+    //   nll-=dnorm(tau(t,0,ev), Type(0), Type(1), true);
+    //   nll-=dnorm(tau(t,1,ev), Type(0), Type(1), true);
+    // }
+    nll-=dnorm(logNatM(t), logM_mu, logM_sigma, true);
+    nll-=dnorm(tau(t), mu_tau, sigma_tau, true);
   }
   
-  vector<Type> pcap_pred(lengths_pred.size());
-  for(int i=0; i<pcap_pred.size(); i++){
-    // The probability of capture as function of length given maximum effort 
-    pcap_pred(i)=max_prob*(1-exp(-k(0,0,0)*Type(3.5)))/(1+exp(-a*(lengths_pred(i)-b)));
-    //      1/(1+exp(-a*(lengths_pred(i)-b)));
-  }
+  // vector<Type> pcap_pred(lengths_pred.size());
+  // for(int i=0; i<pcap_pred.size(); i++){
+  //   // The probability of capture as function of length given maximum effort 
+  //   pcap_pred(i)=1;//max_prob*(1-exp(-k(0,0,0)*Type(3.5)))/(1+exp(-a*(lengths_pred(i)-b)));
+  //   //      1/(1+exp(-a*(lengths_pred(i)-b)));
+  // }
   // vector<Type> catchabilityM_pred(esfuerzo_pred.size());
   // vector<Type> catchabilityH_pred(esfuerzo_pred.size());
   // for(int i=0; i<esfuerzo_pred.size(); i++){
@@ -137,35 +146,43 @@ Type objective_function<Type>::operator() ()
   //   catchabilityH_pred(i)=1-exp(-exp(mu_tauH)*esfuerzo_pred(i));
   // }
   
-  // reportando
-  vector<Type> kM1(K);
-  vector<Type> kM2(K);
-  vector<Type> kM3(K);  
-  vector<Type> kH1(K);
-  vector<Type> kH2(K);
-  vector<Type> kH3(K);  
-  kM1=k.col(0).col(0);
-  kM2=k.col(1).col(0);
-  kM3=k.col(2).col(0);
-  kH1=k.col(0).col(1);
-  kH2=k.col(1).col(1);
-  kH3=k.col(2).col(1);
-  ADREPORT(kM1);
-  ADREPORT(kM2);
-  ADREPORT(kM3);
-  ADREPORT(kH1);
-  ADREPORT(kH2);
-  ADREPORT(kH3);
-  ADREPORT(r);
+  // // reportando
+  // vector<Type> kM1(K);
+  // vector<Type> kM2(K);
+  // vector<Type> kM3(K);  
+  // vector<Type> kH1(K);
+  // vector<Type> kH2(K);
+  // vector<Type> kH3(K);  
+  // kM1=k.col(0).col(0);
+  // kM2=k.col(1).col(0);
+  // kM3=k.col(2).col(0);
+  // kH1=k.col(0).col(1);
+  // kH2=k.col(1).col(1);
+  // kH3=k.col(2).col(1);
+  // ADREPORT(kM1);
+  // ADREPORT(kM2);
+  // ADREPORT(kM3);
+  // ADREPORT(kH1);
+  // ADREPORT(kH2);
+  // ADREPORT(kH3);
+  // ADREPORT(r);
 
-  Type NatMortH=NatM(0);
-  Type NatMortM=NatM(1);
-  ADREPORT(NatMortH);
-  ADREPORT(NatMortM);
+  // Type NatMortH=NatM(0);
+  // Type NatMortM=NatM(1);
+  // ADREPORT(NatMortH);
+  // ADREPORT(NatMortM);
   // ADREPORT(a);
   // ADREPORT(b);
-  ADREPORT(pcap_pred);
+  vector<Type> surv= exp(-NatM);
+  ADREPORT(surv);
+  ADREPORT(NatM);
+  ADREPORT(logNatM);
+  // ADREPORT(pcap_pred);
   ADREPORT(max_prob);
+  Type NatM_mu=exp(logM_mu);
+  ADREPORT(NatM_mu);
+  ADREPORT(pcap);
+  ADREPORT(tau);
   // ADREPORT(catchabilityM_pred);
   // ADREPORT(catchabilityH_pred);
   REPORT(p);
--- run_analysis.R
+++ run_analysis.R
@@ -3,37 +3,86 @@ source("startup.R")
 ## Construye el modelo
 compile('modelos/cjs_jf.cpp')
 dyn.load(dynlib('modelos/cjs_jf'))
-pars <- list(logNatM=c(-7,-7), logr=-10, a=.4, b=90,
-             tau=array(0, dim=c(data$K, 2,3)),
-             mu_tau=matrix(-2, nrow=2,ncol=3), logsigma_tau=1,
-             theta=0)
-map <- list(logr=factor(NA), #a=factor(NA), b=factor(NA),
-            logNatM=factor(c(1,2)))
+pars <- list(logNatM=rep(-5, data$K),
+             logr=-10, a=.4, b=90,
+             tau=rep(1, data$K), #array(0, dim=c(data$K, 2,3)),
+             mu_tau=1,#matrix(-2, nrow=2,ncol=3),
+             logsigma_tau=1,
+             theta=0, logM_mu=-5, logM_sigma=2)
+##a=factor(NA), b=factor(NA), logNatM=factor(c(1,2)))
+## tau=factor(array(NA, dim=c(data$K,2,3))), logsigma_tau=factor(NA),
+map <- list(logr=factor(NA),  a=factor(NA), b=factor(NA), theta=factor(NA))
 obj <- MakeADFun(data=data, parameters=pars, DLL='cjs_jf',
-                 random=c('tau'), map=map)
+                 random=c('tau', 'logNatM'), map=map)
 obj$env$beSilent()
-## opt <- nlminb(obj$par, obj$fn, obj$gr, control=list(trace=1))
-opt <- TMBhelper::Optimize(obj, control=list(trace=10))
+opt <- TMBhelper::Optimize(obj, control=list(trace=1))
 rep <- sdreport(obj)
 est <- data.frame(par=names(rep$value), est=rep$value, se=rep$sd)
 
-tmp <- cbind(periodo=1:data$K,est[grep('k', est$par),])
-tmp$lwr <- pmax(tmp$est-1.96*tmp$se,0)
-tmp$upr <- tmp$est+1.96*tmp$se
-tmp$genero <- substr(tmp$par,2,2)
-tmp$evento <- as.numeric(substr(tmp$par,3,3))
-tmp$periodo <- tmp$periodo + ifelse(tmp$genero=='M',0,.25)
-g <- ggplot(tmp, aes(periodo, y=est, color=genero)) +
-  geom_pointrange(aes(ymin=lwr, ymax=upr), fatten=.5) +
-  facet_wrap('evento', ncol=1) + ylab("k")
-ggsave('plots/k_by_sex_event.png', g, width=7, height=5)
+## Examina la mortalidades
+tmp <- cbind(periodo=1:data$K,est[est$par=='surv',])
+z <- 1
+tmp$lwr <- pmax(tmp$est-z*tmp$se,0)
+tmp$upr <- pmin(tmp$est+z*tmp$se,1)
+g <- ggplot(tmp, aes(periodo, y=est)) +
+  geom_pointrange(aes(ymin=lwr, ymax=upr)) + geom_point() #+ scale_y_log10()
+ggsave('plots/surv_por_periodo.png', g, width=7, height=5)
 
+tmp <- cbind(periodo=1:data$K,est[est$par=='logNatM',])
+z <- 1
+tmp$lwr <- tmp$est-z*tmp$se
+tmp$upr <- tmp$est+z*tmp$se
+g <- ggplot(tmp, aes(periodo, y=est)) +
+  geom_pointrange(aes(ymin=lwr, ymax=upr)) + geom_point() +
+  ylab("logNatM")
+g <- g + coord_cartesian(ylim=c(-20,20))
+ggsave('plots/logNatM_por_periodo.png', g, width=7, height=5)
 
-xx <- droplevels(est[grep('pcap_pred', est$par),])
-plot(data$lengths_pred, xx$est, ylim=c(0,1))
-lines(data$lengths_pred, xx$est+ xx$se*1.96)
-lines(data$lengths_pred, xx$est- xx$se*1.96)
-rug(data$lengths)
+tmp <- cbind(periodo=1:data$K,est[est$par=='tau',])
+z <- 1
+tmp$lwr <- tmp$est-z*tmp$se
+tmp$upr <- tmp$est+z*tmp$se
+g <- ggplot(tmp, aes(periodo, y=est)) +
+  geom_pointrange(aes(ymin=lwr, ymax=upr)) + geom_point() +
+  ylab("logit(sobrevivencia)")
+g <- g + coord_cartesian(ylim=c(-20,20))
+ggsave('plots/tau_por_periodo.png', g, width=7, height=5)
+
+## Examina la mortalidades
+tmp <- cbind(periodo=1:data$K,est[est$par=='surv',])
+z <- 1
+tmp$lwr <- pmax(tmp$est-z*tmp$se,0)
+tmp$upr <- pmin(tmp$est+z*tmp$se,1)
+g <- ggplot(tmp, aes(periodo, y=est)) +
+  geom_pointrange(aes(ymin=lwr, ymax=upr)) + geom_point() +
+  ylab("sobrevivencia")
+ggsave('plots/surv_por_periodo.png', g, width=7, height=5)
+
+## Examina la mortalidades
+tmp <- cbind(periodo=1:data$K,est[est$par=='pcap',])
+z <- 1
+tmp$lwr <- pmax(tmp$est-z*tmp$se,0)
+tmp$upr <- pmin(tmp$est+z*tmp$se,1)
+g <- ggplot(tmp, aes(periodo, y=est)) +
+  geom_pointrange(aes(ymin=lwr, ymax=upr)) + geom_point() +
+  ylab("prob captura")
+ggsave('plots/pcap_por_periodo.png', g, width=7, height=5)
+
+## tmp <- cbind(periodo=1:data$K,est[grep('k', est$par),])
+## tmp$lwr <- pmax(tmp$est-1.96*tmp$se,0)
+## tmp$upr <- tmp$est+1.96*tmp$se
+## tmp$genero <- substr(tmp$par,2,2)
+## tmp$evento <- as.numeric(substr(tmp$par,3,3))
+## tmp$periodo <- tmp$periodo + ifelse(tmp$genero=='M',0,.25)
+## g <- ggplot(tmp, aes(periodo, y=est, color=genero)) +
+##   geom_pointrange(aes(ymin=lwr, ymax=upr), fatten=.5) +
+##   facet_wrap('evento', ncol=1) + ylab("k")
+## ggsave('plots/k_by_sex_event.png', g, width=7, height=5)
+## xx <- droplevels(est[grep('pcap_pred', est$par),])
+## plot(data$lengths_pred, xx$est, ylim=c(0,1))
+## lines(data$lengths_pred, xx$est+ xx$se*1.96)
+## lines(data$lengths_pred, xx$est- xx$se*1.96)
+## rug(data$lengths)
 
 ## par(mfrow=c(3,1))
 ## x <- 1:data$K
